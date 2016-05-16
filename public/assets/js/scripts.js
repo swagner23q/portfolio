b253b2404abbb1c70d6cfc93e6c2403deabff5b6
sara <sara.wagner23q@gmail.com>
2016-05-15 21:35:27 -0700

    add scroll in animations to portfolio samples

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function() {
+  setTimeout(function(){
+      $('body').addClass('loaded');
+  }, 3000);
+  $(window).scroll( function(){
+    /* Check the location of each desired element */
+    $('.hideme').each( function(i){
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        /* If the object is completely visible in the window, fade it in */
+        if( bottom_of_window > bottom_of_object ){
+            $(this).addClass("expandUp");
+        }
+    });
+  });
+});
